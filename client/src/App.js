6d1f16df79d3e2388ebf1dfb8e2ab8c3fbdbff75
ubermensh <lemantar.flcl@gmail.com>
2017-07-29 18:45:21 +0300

    react + express

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { users: []};
+  componentDidMount() {
+      fetch('/users')
+        .then(res => res.json())
+        .then(users => this.setState({ users }));
+    }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )} 
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,20 @@ const googleTrends = require('google-trends-api');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+	googleTrends.interestOverTime({keyword: 'Women\'s march'}, function(err, results){
+	  if(err) console.error('there was an error!', err);
+	  else {
+			res.json(results);
+		}
+	})
   //res.send('respond with a resource');
-  res.json([{
-    id: 1,
-    username: "samsepi0l"
-  }, {
-    id: 2,
-    username: "D0loresH4ze"
-  }]);
+  //res.json([{
+    //id: 1,
+    //username: "samsepi0l"
+  //}, {
+    //id: 2,
+    //username: "D0loresH4ze"
+  //}]);
 
 });
 
