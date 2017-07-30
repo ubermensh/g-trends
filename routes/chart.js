18ed17cac3f4ea4c7496d2933d41fade1d9087fd
ubermensh <lemantar.flcl@gmail.com>
2017-07-30 22:48:46 +0300

    parsing google trends data for chart.js

--- routes/chart.js
+++ routes/chart.js
@@ -1,24 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const debug = require('debug')('chart');
 const googleTrends = require('google-trends-api');
 
 function reformatTrendsData(trendsData) {
-    console.log(trendsData);
+    trendsData = JSON.parse(trendsData);
+    const res = trendsData['default']['timelineData'];
+    const labels = [];
+    const data = [];
+
+    for (let key in res) {
+      let item = res[key];
+      labels.push(item.formattedTime);
+      data.push(parseInt(item.formattedValue));
+    }
     return {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            data: [12, 19, 3, 5, 2, 3],
-        }]
+        labels: labels,
+        datasets: [{data:data}]
     }
 
-   //return trendsData.map(item => {
-     //for (var key in item) {
-       //var obj = {};
-       //obj['date'] = key;
-       //obj['volume'] = item[key];
-       //return obj;
-     //}
-   //});
+    //return {
+        //labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        //datasets: [{
+            //data: [12, 19, 3, 5, 2, 3],
+        //}]
+    //}
 }
 
 router.get('/', function(req, res, next) {
@@ -26,7 +32,7 @@ router.get('/', function(req, res, next) {
     function(err, results){
 	  if(err) console.error('there was an error!', err);
 	  else {
-            var reformatedResults = reformatTrendsData(results[0]);
+            var reformatedResults = reformatTrendsData(results);
 			res.json(reformatedResults);
 		}
 	})
