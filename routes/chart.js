22069d73c5533e7d7ee592f1a91f8c0a570ac3b4
ubermensh <lemantar.flcl@gmail.com>
2017-07-30 21:28:02 +0300

    rename  route

---
+++ routes/chart.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const googleTrends = require('google-trends-api');
+
+router.get('/', function(req, res, next) {
+	googleTrends.interestOverTime({keyword: 'Women\'s march'}, function(err, results){
+	  if(err) console.error('there was an error!', err);
+	  else {
+			res.json(results);
+		}
+	})
+  //res.send('respond with a resource');
+  //res.json([{
+    //id: 1,
+    //username: "samsepi0l"
+  //}, {
+    //id: 2,
+    //username: "D0loresH4ze"
+  //}]);
+
+});
+
+module.exports = router;
